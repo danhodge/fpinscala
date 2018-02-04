deb8fb25e67e1a763fdfdc7614214cc4cc911b93
Dan Hodge <danhodge@gmail.com>
2018-02-03 22:45:20 -0500

    completed exercise 3.3

---
+++ chapter3/Exercise3.scala
@@ -0,0 +1,33 @@
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
+
+object Exercise3 {
+  def main(args: Array[String]): Unit = {
+    val l = List(1, 2, 3, 4, 5)
+    val t = List.setHead(l, 17)
+    println(t)
+  }
+}
